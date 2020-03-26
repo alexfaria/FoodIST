810f80c770274ec0d58c5cf83936f1be7837b075
Rafampinto <rafapinto23@hotmail.com>
2020-03-26 00:50:23 +0000

    Implemented parcelable to be able to put on bundle

--- android/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/model/DiningOption.java
+++ android/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/model/DiningOption.java
@@ -1,9 +1,29 @@
 package pt.ulisboa.tecnico.cmov.foodist.model;
 
-public class DiningOption {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class DiningOption implements Parcelable {
 
     private String name;
     private String openingHours;
+
+    public static final Creator<DiningOption> CREATOR = new Creator<DiningOption>() {
+        @Override
+        public DiningOption createFromParcel(Parcel in) {
+            return new DiningOption(in);
+        }
+
+        @Override
+        public DiningOption[] newArray(int size) {
+            return new DiningOption[size];
+        }
+    };
+
+    private DiningOption(Parcel in) {
+        name = in.readString();
+        openingHours = in.readString();
+    }
 
     public DiningOption(String name, String openingHours) {
         this.name = name;
@@ -16,5 +36,16 @@ public class DiningOption {
 
     public String getOpeningHours() {
         return openingHours;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(openingHours);
     }
 }
