310c3ee336b2ca747012b570e69eafd2c7e89b20
PATX0 <tiagovcsoares@gmail.com>
2020-04-10 01:27:34 +0100

    added onClick to dish photos (fullscreen)

--- android/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/view/fragments/DishFragment.java
+++ android/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/view/fragments/DishFragment.java
@@ -1,20 +1,36 @@
 package pt.ulisboa.tecnico.cmov.foodist.view.fragments;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.maps.MapView;
-
 import pt.ulisboa.tecnico.cmov.foodist.R;
+import pt.ulisboa.tecnico.cmov.foodist.view.animation.ViewWeightAnimation;
 
 public class DishFragment extends Fragment {
+
+    private static final int IMG_LAYOUT_STATE_CONTRACTED = 0;
+    private static final int IMG_LAYOUT_STATE_EXPANDED = 1;
+    private int imgLayoutState = 0;
+
+    private ConstraintLayout foodServiceContainer;
+
+    private ConstraintLayout imagesContainer;
+    private ImageView imgView;
+
+    /*@Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }*/
 
     @Override
     public View onCreateView(
@@ -23,22 +39,65 @@ public class DishFragment extends Fragment {
     ) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dish, container, false);
-        MapView mapView = view.findViewById(R.id.mapView);
-        mapView.setOnClickListener(new View.OnClickListener() {
+        imgView = view.findViewById(R.id.DishPhoto);
+        //imgView.onCreate(savedInstanceState);
+        imgView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
-                        Uri.parse("http://maps.google.com/maps?saddr=20.344,34.34&daddr=20.5666,45.345"));
-                startActivity(intent);
+                if(imgLayoutState == IMG_LAYOUT_STATE_CONTRACTED){
+                    imgLayoutState = IMG_LAYOUT_STATE_EXPANDED;
+                    expandImgAnimation();
+                }
+                else if(imgLayoutState == IMG_LAYOUT_STATE_EXPANDED){
+                    imgLayoutState = IMG_LAYOUT_STATE_CONTRACTED;
+                    contractImgAnimation();
+                }
             }
         });
-        String name = getArguments().getString("name");
         return view;
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    private void expandImgAnimation(){
+        ViewWeightAnimation imgAnimationWrapper = new ViewWeightAnimation(imagesContainer);
+        ObjectAnimator imgAnimation = ObjectAnimator.ofFloat(imgAnimationWrapper,
+                "weight",
+                65,
+                100);
+        imgAnimation.setDuration(800);
+
+        ViewWeightAnimation recyclerAnimationWrapper = new ViewWeightAnimation(foodServiceContainer);
+        ObjectAnimator recyclerAnimation = ObjectAnimator.ofFloat(recyclerAnimationWrapper,
+                "weight",
+                35,
+                0);
+        recyclerAnimation.setDuration(800);
+
+        recyclerAnimation.start();
+        imgAnimation.start();
+    }
+
+    private void contractImgAnimation(){
+        ViewWeightAnimation imgAnimationWrapper = new ViewWeightAnimation(imagesContainer);
+        ObjectAnimator imgAnimation = ObjectAnimator.ofFloat(imgAnimationWrapper,
+                "weight",
+                100,
+                65);
+        imgAnimation.setDuration(800);
+
+        ViewWeightAnimation recyclerAnimationWrapper = new ViewWeightAnimation(foodServiceContainer);
+        ObjectAnimator recyclerAnimation = ObjectAnimator.ofFloat(recyclerAnimationWrapper,
+                "weight",
+                0,
+                35);
+        recyclerAnimation.setDuration(800);
+
+        recyclerAnimation.start();
+        imgAnimation.start();
+    }
+
+    /*public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        /*
+
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,5 +105,5 @@ public class DishFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });*/
-    }
 }
+
