590af128340715f129712c739d11c59e8a4a4aa5
rafampinto <rafapinto23@hotmail.com>
2020-04-11 17:26:51 +0100

    Added DishViewModel

---
+++ android/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/view/viewmodel/DishViewModel.java
@@ -0,0 +1,31 @@
+package pt.ulisboa.tecnico.cmov.foodist.view.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+import pt.ulisboa.tecnico.cmov.foodist.model.Dish;
+import pt.ulisboa.tecnico.cmov.foodist.repository.DishRepository;
+import pt.ulisboa.tecnico.cmov.foodist.view.App;
+
+public class DishViewModel extends ViewModel {
+
+    private DishRepository repository;
+
+    public void init(App application) {
+        repository = new DishRepository(application);
+    }
+
+    public LiveData<List<Dish>> getDishes(String foodServiceName) {
+        return repository.getDishes(foodServiceName);
+    }
+
+    public LiveData<Dish> getDish(String foodServiceName, String name) {
+        return repository.getDish(foodServiceName, name);
+    }
+
+    public void putDish(String foodServiceName, Dish dish) {
+        repository.putDish(foodServiceName, dish);
+    }
+}
