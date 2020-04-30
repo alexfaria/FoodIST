0f4c16dbd0346163def630e1e5381b47d1f7e749
Alex Faria <alexandre.faria@tecnico.ulisboa.pt>
2020-04-30 19:19:08 +0100

    Update FoodServiceFragment to show the menu

--- android/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/view/fragments/FoodServiceFragment.java
+++ android/app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/view/fragments/FoodServiceFragment.java
@@ -22,6 +22,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.preference.PreferenceManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -33,20 +35,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.GeoApiContext;
 
 import pt.ulisboa.tecnico.cmov.foodist.R;
+import pt.ulisboa.tecnico.cmov.foodist.model.Dish;
 import pt.ulisboa.tecnico.cmov.foodist.view.App;
+import pt.ulisboa.tecnico.cmov.foodist.view.adapter.FoodMenuAdapter;
 import pt.ulisboa.tecnico.cmov.foodist.view.animation.ViewWeightAnimation;
+import pt.ulisboa.tecnico.cmov.foodist.view.viewmodel.DishViewModel;
 import pt.ulisboa.tecnico.cmov.foodist.view.viewmodel.FoodServiceViewModel;
 
 public class FoodServiceFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
-    private static final String TAG = "directions";
     private static final int MAP_LAYOUT_STATE_CONTRACTED = 0;
     private static final int MAP_LAYOUT_STATE_EXPANDED = 1;
     private static final int MAP_ZOOM_LEVEL = 17;
 
     // FoodServiceInfo
     private ConstraintLayout foodServiceContainer;
-    private TextView name;
-    private TextView openingHours;
 
     // Maps
     private int mapLayoutState = 0;
@@ -55,15 +57,27 @@ public class FoodServiceFragment extends Fragment implements OnMapReadyCallback,
     private GoogleMap googleMap;
     private GeoApiContext mGeoApiContext = null;
 
-    private FoodServiceViewModel viewModel;
+    private FoodServiceViewModel foodServiceViewModel;
     private SharedPreferences sharedPreferences;
+
+    // Dish
+    private RecyclerView recyclerView;
+    private FoodMenuAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private DishViewModel dishViewModel;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (isAdded()) {
-            viewModel = new ViewModelProvider(requireActivity()).get(FoodServiceViewModel.class);
-            viewModel.init((App) getContext().getApplicationContext());
+            foodServiceViewModel = new ViewModelProvider(requireActivity()).get(FoodServiceViewModel.class);
+            foodServiceViewModel.init((App) getContext().getApplicationContext());
+
+            dishViewModel = new ViewModelProvider(requireActivity()).get(DishViewModel.class);
+            dishViewModel.init((App) getContext().getApplicationContext());
+
             sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireActivity());
         }
     }
@@ -77,11 +91,9 @@ public class FoodServiceFragment extends Fragment implements OnMapReadyCallback,
         View view = inflater.inflate(R.layout.fragment_food_service, container, false);
 
         foodServiceContainer = view.findViewById(R.id.foodServiceContainer);
-        name = view.findViewById(R.id.foodServiceName);
-        openingHours = view.findViewById(R.id.foodServiceOpening);
-
         mapContainer = view.findViewById(R.id.mapContainer);
         mapView = view.findViewById(R.id.foodServiceMap);
+
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
         view.findViewById(R.id.fullScreenBtn).setOnClickListener(v -> {
@@ -93,13 +105,28 @@ public class FoodServiceFragment extends Fragment implements OnMapReadyCallback,
                 contractMapAnimation();
             }
         });
-        view.findViewById(R.id.menuBtn).setOnClickListener(v -> {
-            Bundle args = new Bundle();
-            args.putString("foodServiceName", name.getText().toString());
+
+        adapter = new FoodMenuAdapter(v -> {
+            Bundle args = getArguments();
+            TextView dishName = v.findViewById(R.id.dishName);
+            args.putString("dishName", dishName.getText().toString());
             NavHostFragment
                     .findNavController(FoodServiceFragment.this)
-                    .navigate(R.id.action_FoodService_to_FoodMenu, args);
+                    .navigate(R.id.action_FoodMenu_to_Dish, args);
         });
+
+        recyclerView = view.findViewById(R.id.menu_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+        layoutManager = new LinearLayoutManager(this.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        view.findViewById(R.id.addMenuBtn).setOnClickListener(v -> {
+            NavHostFragment
+                    .findNavController(FoodServiceFragment.this)
+                    .navigate(R.id.action_FoodMenu_to_AddToMenu, getArguments());
+        });
+
         return view;
     }
 
@@ -111,9 +138,7 @@ public class FoodServiceFragment extends Fragment implements OnMapReadyCallback,
             String campus = sharedPreferences.getString("campus", getString(R.string.default_campus));
             String status = sharedPreferences.getString("status", getString(R.string.default_status));
 
-            viewModel.getFoodService(campus, status, foodServiceName).observe(this, fs -> {
-                name.setText(fs.getName());
-                openingHours.setText(fs.getOpeningHours());
+            foodServiceViewModel.getFoodService(campus, status, foodServiceName).observe(this, fs -> {
                 if (googleMap != null) {
                     LatLng foodServiceCoord = new LatLng(fs.getLatitude(), fs.getLongitude());
                     MarkerOptions marker = new MarkerOptions().position(foodServiceCoord);
@@ -123,6 +148,13 @@ public class FoodServiceFragment extends Fragment implements OnMapReadyCallback,
                     googleMap.addMarker(marker).showInfoWindow();
                     googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(foodServiceCoord, MAP_ZOOM_LEVEL));
                 }
+            });
+
+            dishViewModel.getDishes(foodServiceName).observe(this, dishes -> {
+                for(Dish dish : dishes) {
+                    Log.d("dishes", String.valueOf(dish));
+                }
+                adapter.setData(dishes);
             });
         }
     }
@@ -228,7 +260,7 @@ public class FoodServiceFragment extends Fragment implements OnMapReadyCallback,
                                 startActivity(mapIntent);
                             }
                         } catch (NullPointerException e) {
-                            Log.e(TAG, "onClick: NullPointerException: Couldn't open map." + e.getMessage());
+                            Log.e("directions", "onClick: NullPointerException: Couldn't open map." + e.getMessage());
                             Toast.makeText(getActivity(), "Couldn't open map", Toast.LENGTH_SHORT).show();
                         }
                     }
